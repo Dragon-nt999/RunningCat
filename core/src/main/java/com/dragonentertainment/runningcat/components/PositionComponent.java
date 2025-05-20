a3583cb52c1caf2b6f173736d9757d56e5eb2364
Dragon Pham <dragonpham.nt9989@gmail.com>
2025-05-21 01:11:51 +0700

    Comple ESC

--- core/src/main/java/com/dragonentertainment/runningcat/components/PositionComponent.java
+++
@@ -1,7 +0,0 @@
-package com.dragonentertainment.runningcat.components;
-
-import com.badlogic.ashley.core.Component;
-
-public class PositionComponent implements Component {
-    public float x = 0, y = 0;
-}
