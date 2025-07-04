9e53808bb8da89669024679d6431e99d798bf8c7
Dragon-nt999 <dragonpham.nt9989@gmail.com>
2025-07-04 18:35:06 +0700

    Build level in game

--- core/src/main/java/com/dragonentertainment/runningcat/systems/brick/BrickCreateSystem.java
+++ core/src/main/java/com/dragonentertainment/runningcat/systems/brick/BrickCreateSystem.java
@@ -1,12 +1,17 @@
 package com.dragonentertainment.runningcat.systems.brick;
 
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.dragonentertainment.runningcat.components.TransformComponent;
 import com.dragonentertainment.runningcat.components.brick.BrickComponent;
+import com.dragonentertainment.runningcat.enums.Level;
 import com.dragonentertainment.runningcat.factory.BrickFactory;
 import com.dragonentertainment.runningcat.utils.GameGrid;
 import com.dragonentertainment.runningcat.utils.RandomMatrixPositions;
@@ -24,28 +29,29 @@ public class BrickCreateSystem extends EntitySystem {
 
     @Override
     public void addedToEngine(Engine engine) {
-        this.generateBricks(false);
+        this.generateBricks(false, Level.EASY);
     }
 
     @Override
     public void update(float deltaTime) {
-        int brickRemain = this.engine.getEntitiesFor(Family.one(BrickComponent.class).get()).size();
-        if( brickRemain < 36 ) {
-            this.generateBricks(true);
+        ImmutableArray<Entity> allBricks = this.engine.getEntitiesFor(Family.one(BrickComponent.class).get());
+        Entity lastBrick = allBricks.get(allBricks.size() - 1);
+        int lastX = (int)lastBrick.getComponent(TransformComponent.class).position.x;
+
+        if( lastX < GameGrid.WORLD_WIDTH / 5) {
+            this.generateBricks(true, Level.EASY);
         }
     }
 
-    private void generateBricks(boolean isReSpawn) {
-       List<List<Vector2>> randPositions =
-                                    RandomMatrixPositions.getBlockPositions(5, isReSpawn);
+    private void generateBricks(boolean isReSpawn, Level level) {
+       List<List<Vector2>> randPositions = RandomMatrixPositions.getBlockPositions(isReSpawn, level);
         for(int i = 0; i < randPositions.size(); i++) {
             List<Vector2> positions = randPositions.get(i);
             for(int j = 0; j < positions.size(); j++) {
-                int w = isReSpawn ? GameGrid.WORLD_WIDTH : 0;
                 BrickFactory.createBrick(
                     this.engine,
                     this.texture,
-                    positions.get(j).x + w,
+                    positions.get(j).x,
                     positions.get(j).y,
                     9);
             }
